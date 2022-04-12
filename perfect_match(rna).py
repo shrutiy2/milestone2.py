355879c6ff1497960ffc09bf522f16d8c5c4d857
cwiberg2 <100723004+cwiberg2@users.noreply.github.com>
2022-04-12 15:05:47 -0500

    Update perfect_match(rna).py

--- perfect_match(rna).py
+++ perfect_match(rna).py
@@ -1,9 +1,11 @@
 from math import factorial
 def perfect_match(rna):
-    rna = rna.upper()
-    c_count = rna.count('C')
+    rna = rna.upper() #formatting input for consistency
+    c_count = rna.count('C') #counts for each base in the rna strand
+    g_count = rna.count('G')
     a_count = rna.count('A')
-    matches = factorial(c_count)*factorial(a_count)
-    if len(rna)%2 != 0:
+    u_count = rna.count('U')
+    matches = factorial(c_count)*factorial(a_count) #there are n! ways to pair n C's with G's. The same is true for A and U. Multiplying these together gives the number of ways to arrange these arrangements.
+    if c_count != g_count or a_count != u_count: #if there is not a G for every C or A for every U a perfect match is impossible
         matches = 0
     return matches
